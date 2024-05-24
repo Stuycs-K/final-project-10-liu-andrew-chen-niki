15d673cd426ac1be6143ebb504a0d84ee0836f94
an6reww <an6reww@gmail.com>
2024-05-24 15:03:13 -0400

    condense for loop

--- methods.py
+++ methods.py
@@ -3,12 +3,7 @@ def encode(message ,keyMatrix):
     print(message)
     message = [char%65 for char in message]
     print(message)
-    # message = [ for i in range((int)(len(message)/3 + 1))]
-
-    triplets = []
-    # slice the message string into smaller string of size 3
-    for i in range((int)(len(message)/3 + 1)):
-        triplets.append(message[i * 3:(min((i+1) * 3, len(message)))])
+    triplets = [message[i:i+3] for i in range (0,len(message),3)]
     print(triplets)
 
 encode("ASFLJDA", 0 )
