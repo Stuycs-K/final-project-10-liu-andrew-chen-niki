3fab299929d53230d7b1dad40e78591f9eba8d3a
an6reww <an6reww@gmail.com>
2024-05-30 15:05:15 -0400

    generalize to n

--- methods.py
+++ methods.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def encode(message: str ,keyMatrix: list) -> str:
     keyMatrix = np.array(keyMatrix)
-    return_string = ""
+    size = len(keyMatrix)
     print(f'Original Message: {message}')
     # store message as their ascii values
     message = [ord(char) for char in message]
@@ -11,20 +11,20 @@ def encode(message: str ,keyMatrix: list) -> str:
     message = [char%65 for char in message]
     print(f'Taking Modulos 65 {message}')
     # split into triplets
-    triplets = [message[i:i+3] for i in range (0,len(message),3)]
-    print(f'Splitting into Triplets: {triplets}')
+    n_plets = [message[i:i+size] for i in range (0,len(message),size)]
+    print(f'Splitting into {size}-plets: {n_plets}')
     # pad triplets with zeros 
-    triplets = [triplet + [0] * (3-len(triplet)) for triplet in triplets]
-    print(f'Padding triplets with zeros: {triplets}')
+    n_plets = [n_plet + [0] * (3-len(n_plet)) for n_plet in n_plets]
+    print(f'Padding {size}-plets with zeros: {n_plets}')
     # convert into np 
-    triplets = [np.array(triplet) for triplet in triplets]
+    n_plets = [np.array(n_plet) for n_plet in n_plets]
     # multiply each triplet with the keyMatrix
     encoded_message = []
-    for index, triplet in enumerate(triplets):
+    for index, triplet in enumerate(n_plets):
         column = np.transpose(triplet)
         encoded_product = np.dot(keyMatrix, column)%26
         encoded_triplet = encoded_product.flatten().tolist()
-        print(f'Triplet {index}: {encoded_triplet}')
+        print(f'{size}-plets {index}: {encoded_triplet}')
         encoded_message += encoded_triplet
     
     encoded_message = [chr(value + 65) for value in encoded_message]
