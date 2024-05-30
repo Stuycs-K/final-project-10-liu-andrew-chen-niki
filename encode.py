4dd5102d50fc29c0cbf506ffe48c226501fd75a0
an6reww <an6reww@gmail.com>
2024-05-30 10:35:58 -0400

    Allow user input to work with encoding, demonstrate the process of encoding through print statements.

--- encode.py
+++ encode.py
@@ -2,10 +2,11 @@ import sys
 import methods
 
 inputTextFile = sys.argv[1]
+message = sys.argv[2]
 
 with open(inputTextFile,'r') as f:
     lines = f.readlines()
 
 keyMatrix = [[int(num) for num in line.split()] for line in lines]
 
-print(keyMatrix)
+print(methods.encode(message, keyMatrix))
--- methods.py
+++ methods.py
@@ -3,27 +3,28 @@ import numpy as np
 def encode(message: str ,keyMatrix: list) -> str:
     keyMatrix = np.array(keyMatrix)
     return_string = ""
+    print(f'Original Message: {message}')
     # store message as their ascii values
     message = [ord(char) for char in message]
-    print(message)
+    print(f'Converting Into ASCII: {message}')
     # 0-25 the ascii values
     message = [char%65 for char in message]
-    print(message)
+    print(f'Taking Modulos 65 {message}')
     # split into triplets
     triplets = [message[i:i+3] for i in range (0,len(message),3)]
-    print(triplets)
+    print(f'Splitting into Triplets: {triplets}')
     # pad triplets with zeros 
     triplets = [triplet + [0] * (3-len(triplet)) for triplet in triplets]
-    print(triplets)
+    print(f'Padding triplets with zeros: {triplets}')
     # convert into np 
     triplets = [np.array(triplet) for triplet in triplets]
     # multiply each triplet with the keyMatrix
     encoded_message = []
-    for triplet in triplets:
+    for index, triplet in enumerate(triplets):
         column = np.transpose(triplet)
         encoded_product = np.dot(keyMatrix, column)%26
         encoded_triplet = encoded_product.flatten().tolist()
-        # print(encoded_triplet)
+        print(f'Triplet {index}: {encoded_triplet}')
         encoded_message += encoded_triplet
     
     encoded_message = [chr(value + 65) for value in encoded_message]
@@ -31,11 +32,5 @@ def encode(message: str ,keyMatrix: list) -> str:
     encoded_string = "".join(encoded_message)
 
     return encoded_string
-
-
-
-
-encode("ASFLJDA", [[6,24,1],[13,16,10],[20,17,15]])
-
 
 
